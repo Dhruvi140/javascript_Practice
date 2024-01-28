3ce8f0c0726870bdac240d7560436af595ed1dca
Dhruvi140 <154051978+Dhruvi140@users.noreply.github.com>
2024-01-28 12:22:57 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 3) we are using nodejs ,not browser
+console.log(3 + 3) ,
+ console.log("dhruvi")
+
+ let name = "hitesh";  //string
+ let age =18;  //number
+ let isLogging = true; //false
+//PRIMITIVE Datatype
+
+ //Number => 2 to power 53 (memmory address)
+ //bigInt
+ // string => ""
+ // boolean =>true/false
+ //null => starndalone value
+ // undefined => 
+ //symbol => for uniqueness 
+
+ //OBJECT
+ console.log(typeof null);
+ console.log(typeof undefined);
